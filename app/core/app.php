5c4337ccd1b7aa854154082a9f70de0a3731dfcd
Manolo Cabrera <viprestal@hotmail.com>
2022-02-19 18:46:21 +0100

    refactoring app 2

--- app/core/app.php
+++ app/core/app.php
@@ -8,66 +8,17 @@ class App
     protected $method  = "index";
     protected $params  = array();
     protected $controller_path = null;
+    protected $URL = HOME;
 
     public function __construct()
     {
 
-       $URL = $this->getURL();
+       $this->URL = $this->getURL();
+       $this->controllerExists();
+       $this->loadMethod();
 
-       if(empty($URL[0])){
-        if (file_exists(APPDIR . "Controllers/" . ucfirst(HOME) . "Controller.php")) {
-            $this->controller = ucfirst(HOME) . "Controller";
+    }
 
-            $controller_path = '\app\Controllers';
-            $controller = $this->controller;
-            $load_class = "$controller_path\\$controller";
-            $this->controller = new $load_class;
-            }
-        }
-
-      if($URL[0] === "login"){
-         $this->controller_path = "auth";
-      }
-
-        if (is_null($this->controller_path)) {
-            if (file_exists(APPDIR . "Controllers/" . ucfirst($URL[0]) . "Controller.php")) {
-                $this->controller = ucfirst($URL[0]) . "Controller";
-                unset($URL[0]);
-                $this->loadnamespace();
-
-
-            }
-        }else{
-
-            if(file_exists(APPDIR."Controllers/".$this->controller_path."/".ucfirst($URL[0])."Controller.php"))
-            {
-            $this->controller = ucfirst($URL[0])."Controller";
-
-            $this->loadnamespace($this->controller_path);
-            }
-        }
-
-
-
-        if (isset($URL[1])) {
-            if (method_exists($this->controller, $URL[1])) {
-
-                $this->method = ucfirst($URL[1]);
-                unset($URL[1]);
-            }
-        }
-        $URL = array_values($URL);
-        $this->params = $URL;
-        call_user_func_array([$this->controller, $this->method], $this->params);
-
-}
-
-
-
-/*
-    esta funcion es para obtener la url limpia y todos los parametros
-
-*/
     private function getURL()
     {
         /*
@@ -77,34 +28,73 @@ class App
         */
         $url = isset($_GET['url']) ? $_GET['url'] : HOME;
 
-           /**
-     * Array
-     * Esto es lo que devuelve con el explode y filter_var limpia la
-     * url y cada segmento lo pone en un array
-        (
-        [0] => asas
-        [1] => asa s
-        [2] => re
-        )
-     */
+
         return explode("/", filter_var(trim($url, "/")), FILTER_SANITIZE_URL);
     }
 
     private function loadnamespace($subdir = null)
     {
-                if(!is_null($subdir)){
-                    $controller_path = '\app\Controllers\\'.$subdir;
-                    $controller = $this->controller;
-                    $load_class = "$controller_path\\$controller";
-                    $this->controller = new $load_class;
+        if(!is_null($subdir)){
+            $controller_path = '\app\Controllers\\'.$subdir;
 
+        }else{
+            $controller_path = '\app\Controllers';
+        }
+        $controller = $this->controller;
+        $load_class = "$controller_path\\$controller";
+        $this->controller = new $load_class;
+    }
 
-                }else{
-                    $controller_path = '\app\Controllers';
-                    $controller = $this->controller;
-                    $load_class = "$controller_path\\$controller";
-                    $this->controller = new $load_class;
+    private function completeControllerName ($partial = null)
+    {
+        $this->controller = ucfirst($this->URL[0]) . "Controller";
+        unset($this->URL[0]);
+        return $this;
+    }
+
+    private function controllerExists()
+    {
+        match($this->URL[0]){
+            empty($this->URL[0]) => $data = ucfirst(HOME),
+        };
+
+        if(empty($this->URL[0])){
+            if (file_exists(APPDIR . "Controllers/" . ucfirst(HOME) . "Controller.php")) {
+                $this->completeControllerName()->loadnamespace();
                 }
+            }
 
+          if($this->URL[0] === "login"){
+             $this->controller_path = "auth";
+          }
+            if (is_null($this->controller_path)) {
+                if (file_exists(APPDIR . "Controllers/" . ucfirst($this->URL[0]) . "Controller.php")) {
+
+                    $this->completeControllerName()->loadnamespace();
+                }
+            }else{
+
+                if(file_exists(APPDIR."Controllers/".$this->controller_path."/".ucfirst($this->URL[0])."Controller.php"))
+                {
+
+                $this->completeControllerName()->loadnamespace($this->controller_path);
+                }
+            }
+            return $this;
+    }
+
+    private function loadMethod()
+    {
+        if (isset($this->URL[1])) {
+            if (method_exists($this->controller, $this->URL[1])) {
+
+                $this->method = ucfirst($this->URL[1]);
+                unset($this->URL[1]);
+            }
+        }
+        $URL = array_values($this->URL);
+        $this->params = $URL;
+        call_user_func_array([$this->controller, $this->method], $this->params);
+        return $this;
     }
 }
