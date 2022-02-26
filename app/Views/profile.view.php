f02faa04895b8f41cc33409d948687545b2fd4b2
Manolo Cabrera <viprestal@hotmail.com>
2022-02-26 20:09:07 +0100

    episodio 12

--- app/Views/profile.view.php
+++ app/Views/profile.view.php
@@ -4,12 +4,21 @@
 
     <div class="container-fluid p-4 mt-3 shadow mx-auto" style="max-width: 1000px;">
     <?php $this->view('includes.breadcrumb'); ?>
-        <h4>Profile</h4>
+
         <div class="row">
-            <div class="col-4">
-                <img src="<?=ROOT?>img/" alt="">
+            <div class="col-2">
+                <img src="<?=ROOT?>img/user_female.jpg" alt="" class="border border-primary d-block mx-auto rounded-circle" style="width: 150px;">
             </div>
-            <div class="col-8 bg-secondary">dssdsdfsfsfsf</div>
+            <div class="col-10 bg-light p-2">
+                <table class="table table-hover table-striped table-bordered">
+                    <tr><th>First Name</th><td>Mary</td></tr>
+                    <tr><th>Last Name</th><td>Mary Pili</td></tr>
+                    <tr><th>Gender</th><td>Female</td></tr>
+                </table>
+            </div>
+        </div>
+        <div class="">
+
         </div>
     </div>
 
